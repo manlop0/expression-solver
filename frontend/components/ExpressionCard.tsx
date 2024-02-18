821797a0efc6a2724926959a1facb947e9395226
manlop0 <132251106+manlop0@users.noreply.github.com>
2024-02-19 00:09:02 +0500

    .

--- frontend/components/ExpressionCard.tsx
+++ frontend/components/ExpressionCard.tsx
@@ -13,23 +13,20 @@ type Data = {
 };
 
 export default function ExpressionCard({ data }: { data: Data }) {
-  console.log(data.date)
-  const d = new Date(data.date)
-  console.log(d)
-  console.log(d.toLocaleDateString())
+  const d = new Date(data.date);
   return (
-    <div
-      className="flex max-w-xl w-fit items-center min-h-[100px] min-w-[320px] h-fit py-2 px-6 bg-secondary rounded-xl gap-4 shadow-lg"
-    >
+    <div className="flex max-w-xl w-fit items-center min-h-[100px] min-w-[320px] h-fit py-2 px-6 bg-secondary rounded-xl gap-4 shadow-lg">
       {data.status === 0 ? (
         <Queued />
       ) : data.status === 1 ? (
         <Spinner />
       ) : (
-        <FontAwesomeIcon icon={faCheckCircle} fontSize={48} color="#ffffff"/>
+        <FontAwesomeIcon icon={faCheckCircle} fontSize={48} color="#ffffff" />
       )}
       <div className="flex flex-col w-full items-center">
-        <div className=" text-xl">{`${data.value}=${data.result ? data.result : "?"}`}</div>
+        <div className=" text-xl">{`${data.value}=${
+          data.result ? data.result : "?"
+        }`}</div>
         <div className="text-gray-200 text-base">
           {data.status === 0 ? (
             <p>in Queue...</p>
